c8059b60b2bb7b48fa8ec387297204f084917721
Allen Geiger <geiger.allen@gmail.com>
2020-12-11 17:24:25 -0900

    Initial commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var iconLibrary2ED14D8B4AED4D3384A40EAC399CE900: IVisualPlugin;
+export default iconLibrary2ED14D8B4AED4D3384A40EAC399CE900;
---
+++ .tmp/build/src/settings.d.ts
@@ -0,0 +1,8 @@
+import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
+import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
+export declare class VisualSettings extends DataViewObjectsParser {
+    icon: iconSettings;
+}
+export declare class iconSettings {
+    icon: string;
+}
